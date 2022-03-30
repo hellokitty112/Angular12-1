3fb53b27d2599058599e4bd8176c3deac57d5383
Son <yangsontran69@gmail.com>
2022-03-30 21:11:18 +0700

    DataBinding-ngif

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public name = 'Sơn';
+  public age = 20;
 
   constructor() { }
 
